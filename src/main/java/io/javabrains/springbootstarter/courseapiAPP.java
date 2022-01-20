4c63f58b4acc34ad0546838b5fa6dfce7f706421
iammanvith48 <97126491+iammanvith48@users.noreply.github.com>
2022-01-20 20:04:49 +0530

    Add files via upload

---
+++ src/main/java/io/javabrains/springbootstarter/courseapiAPP.java
@@ -0,0 +1,14 @@
+package io.javabrains.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class courseapiAPP {
+
+	public static void main(String[] args) {
+		SpringApplication.run(courseapiAPP.class, args);
+
+	}
+
+}
